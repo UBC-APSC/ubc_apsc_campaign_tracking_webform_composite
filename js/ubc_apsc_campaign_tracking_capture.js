97ffcced70132285d425f98241450a77c9a33cfd
apsc-web <31107928+apsc-web@users.noreply.github.com>
2025-04-04 08:25:20 -0700

    Update ubc_apsc_campaign_tracking_capture.js
    check for consent

--- js/ubc_apsc_campaign_tracking_capture.js
+++ js/ubc_apsc_campaign_tracking_capture.js
@@ -2,34 +2,37 @@
   Drupal.behaviors.captureUtm = {
     attach: function (context, settings) {
 
-      var cookie_consent = CookiebotCallback_OnAccept();
+      $(document).ready(function () {
+        var cookie_consent = CookiebotCallback_OnAccept();
 
-      // Store UTM parameters + clid in cookies if they exist in the URL
-      const utmParams = drupalSettings.ubc_apsc_campaign_tracking_webform_composite.vars;
+        // Store UTM parameters + clid in cookies if they exist in the URL
+        const utmParams = drupalSettings.ubc_apsc_campaign_tracking_webform_composite.vars;
 
-      utmParams.forEach(param => {
-        const value = getUTMParameter(param);
-        if (value && cookie_consent) {
-          setCookie('act_' + param, value, 365);
+        utmParams.forEach(param => {
+          const value = getUTMParameter(param);
+          if (value && cookie_consent) {
+            setCookie('act_' + param, value, 365);
+          }
+        });
+
+        // Set referrer cookie if not already available and different from local host
+        if (!getCookie('act_' + 'document_referrer') && cookie_consent) {
+
+          let referrer = document.referrer;
+          let currentHost = window.location.hostname;
+
+          if (referrer) {
+            // Parse the referrer URL
+            let referrerHost = new URL(referrer).hostname;
+
+            // Check if the referrer domain is different from the current host
+            if (referrerHost !== currentHost) {
+              setCookie('act_' + 'document_referrer', document.referrer, 365);
+            }
+          }
         }
       });
 
-      // Set referrer cookie if not already available and different from local host
-      if (!getCookie('act_' + 'document_referrer') && cookie_consent) {
-
-        let referrer = document.referrer;
-        let currentHost = window.location.hostname;
-
-        if (referrer) {
-          // Parse the referrer URL
-          let referrerHost = new URL(referrer).hostname;
-
-          // Check if the referrer domain is different from the current host
-          if (referrerHost !== currentHost) {
-            setCookie('act_' + 'document_referrer', document.referrer, 365);
-          }
-        }
-      }
 
       // Function to get UTM parameters from the URL
       function getUTMParameter(name) {
