613c20acda60d6fef87a280a621b9501081ab80c
apsc-web <31107928+apsc-web@users.noreply.github.com>
2025-04-14 10:57:53 -0700

    update script and cookie handling

--- js/ubc_apsc_campaign_tracking_capture.js
+++ js/ubc_apsc_campaign_tracking_capture.js
@@ -44,7 +44,7 @@
     const date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
-    document.cookie = `${name}=${value}; ${expires}; path=/`;
+    document.cookie = `${name}=${value}; ${expires}; path=/; SameSite=Lax`;
   }
 
   // Function to clear cookies
